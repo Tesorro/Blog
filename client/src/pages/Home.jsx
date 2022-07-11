c6f22c5665007a52bdaaee404b2810d02741b615
Tesorro <akarlssin@mail.ru>
2022-07-11 10:03:15 +0300

    show full post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,16 +7,21 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   
   const dispatch = useDispatch();
   const { posts, tags } = useSelector(state => state.posts)
 
-  const isPostsLoading = posts.loading === 'loading';
+  const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
+
   useEffect(() => {
     dispatch(fetchPosts())
+    dispatch(fetchTags())
+
   }, [])
 
   return (
@@ -32,9 +37,9 @@ export const Home = () => {
               <Post key={index} isLoading={true}/>
             ) : (
               <Post
-                id={1}
-                title="Roast the code #1 | Rock Paper Scissors"
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
@@ -45,7 +50,7 @@ export const Home = () => {
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
